80238a3ae4ddd6d8f37cb04b94d5394563cb3157
redone007 <clroyan01@gmail.com>
2019-11-06 14:40:07 +0900

    Create junit project

---
+++ src/main/java/boot/jpa/junit/JunitApplication.java
@@ -0,0 +1,13 @@
+package boot.jpa.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boot/jpa/junit/JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.jpa.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
