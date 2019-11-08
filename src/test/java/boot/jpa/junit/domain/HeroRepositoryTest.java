a551da596f6542c894cf11be5285ef09d041e7ff
redone007 <clroyan01@gmail.com>
2019-11-08 14:40:18 +0900

    Create repository test - JPA auditing

---
+++ src/test/java/boot/jpa/junit/domain/HeroRepositoryTest.java
@@ -0,0 +1,45 @@
+package boot.jpa.junit.domain;
+
+import boot.jpa.junit.domain.hero.Hero;
+import boot.jpa.junit.domain.hero.HeroRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeroRepositoryTest {
+
+    @Autowired
+    private HeroRepository heroRepository;
+
+    @After
+    public void cleanUp() {
+        heroRepository.deleteAll();
+    }
+
+    @Test
+    public void JpaAuditingTest() {
+        //given
+        LocalDateTime now = LocalDateTime.parse("2019-11-08T00:00:00");
+
+        Hero input = Hero.builder()
+                .name("github.com/hong-il")
+                .age(26)
+                .note("github.com/hong-il")
+                .build();
+
+        //when
+        Hero output = heroRepository.save(input);
+
+        //then
+        assertTrue(output.getCreatedDate().isAfter(now));
+        assertTrue(output.getModifiedDate().isAfter(now));
+    }
+}
