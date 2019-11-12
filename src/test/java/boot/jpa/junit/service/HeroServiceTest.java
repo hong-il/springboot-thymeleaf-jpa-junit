f92fa05b20ce229f3bad8972cdb943333da65a5c
redone007 <clroyan01@gmail.com>
2019-11-12 16:19:35 +0900

    Create service test - read

--- src/test/java/boot/jpa/junit/service/HeroServiceTest.java
+++ src/test/java/boot/jpa/junit/service/HeroServiceTest.java
@@ -1,6 +1,7 @@
 package boot.jpa.junit.service;
 
 import boot.jpa.junit.domain.hero.HeroRepository;
+import boot.jpa.junit.dto.HeroFindAllResponseDto;
 import boot.jpa.junit.dto.HeroSaveRequestDto;
 import org.junit.After;
 import org.junit.Test;
@@ -8,6 +9,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -41,5 +45,24 @@ public class HeroServiceTest {
 
         //then
         assertThat(output, is(1L));
+    }
+
+    @Test
+    public void HeroFindAllResponseTest() {
+        //given
+        HeroSaveRequestDto input = HeroSaveRequestDto.builder()
+                .name("github.com/hong-il")
+                .age(26)
+                .note("github.com/hong-il")
+                .build();
+
+        IntStream.rangeClosed(1, 10).forEach(i ->
+                heroService.HeroSaveRequest(input));
+
+        //when
+        List<HeroFindAllResponseDto> output = heroService.HeroFindAllResponse();
+
+        //then
+        assertThat(output.size(), is(10));
     }
 }
